af3389d7670fc163be4c006ec168e784e3807b82
*Afthab <afthabsiyadslrz4@gmail.com>
2023-10-21 16:48:51 +0530

    final commit

--- src/Sections/Services.jsx
+++ src/Sections/Services.jsx
@@ -1,8 +1,12 @@
-const Services = () => {
+import {services} from '../Contants'
+import ServiceCard from '../Components/ServiceCard'
+const Services = () => { 
   return (
-    <div>
-      
-    </div>
+    <section className="max-container flex justify-center flex-wrap gap-9">
+      {services.map((service)=>(
+          <ServiceCard key={service.label} {...service}/>
+      ))}
+    </section>
   )
 }
 
